395e6c49ad1ee5fafeb5d1e256f97e62e1a1976f
praful goyal <prafulgoyal2009@gmail.com>
2015-09-22 14:52:36 -0700

    Initial Version(1.0)

---
+++ app/src/main/java/com/example/family/foodi/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.family.foodi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolBar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolBar);
+        startActivity(new Intent(MainActivity.this, WelcomeScreen.class));
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+
+}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/family/foodi/SplashScreen.java
@@ -0,0 +1,33 @@
+package com.example.family.foodi;
+
+/**
+ * Created by family on 21-09-2015.
+ */
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //thread for splash screen running
+        Thread logoTimer = new Thread() {
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    Log.d("Exception", "Exception" + e);
+                } finally {
+                    startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                }
+                finish();
+            }
+        };
+        logoTimer.start();
+    }
+
+}
